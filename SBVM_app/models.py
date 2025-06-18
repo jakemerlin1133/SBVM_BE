6960e971da931fa007b9b82ad99522ec09548417
jakemerlin1133 <jakemerlin123@gmail.com>
2025-06-18 21:18:13 +0800

    fixed

--- SBVM_app/models.py
+++ SBVM_app/models.py
@@ -4,10 +4,9 @@ from django.db import models
 
 class Student(models.Model):
     student_id = models.CharField(max_length=20, unique=True)
+    password= models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     bottle_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
